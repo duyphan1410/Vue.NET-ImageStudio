efa7da80b89a1e2e2eb9bd17da54ae7fefd670a6
duyphan1410 <tienduy1410@gmail.com>
2025-09-11 16:46:45 +0700

    test

--- BackendApi/obj/Debug/net8.0/BackendApi.AssemblyInfo.cs
+++ BackendApi/obj/Debug/net8.0/BackendApi.AssemblyInfo.cs
@@ -13,9 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79df2e0a1d79c797851b349ecc58e994c0605a7a")]
-
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b752e5e0f4b4c09bbb26fa8bda81ff46da206b5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
