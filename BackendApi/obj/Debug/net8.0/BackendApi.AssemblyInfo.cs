e45dbfdf20e4c8e7ac53ad94ddf91c76343d0091
duyphan1410 <tienduy1410@gmail.com>
2025-09-11 11:53:06 +0700

    add fastapi

--- BackendApi/obj/Debug/net8.0/BackendApi.AssemblyInfo.cs
+++ BackendApi/obj/Debug/net8.0/BackendApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79df2e0a1d79c797851b349ecc58e994c0605a7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
